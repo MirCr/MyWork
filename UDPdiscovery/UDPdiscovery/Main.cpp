807b80fe9ad59275900fd63e09e10ac0f5739246
MirkoCr <90mirko90@gmail.com>
2018-08-30 20:06:14 +0200

    deleted comments

--- UDPdiscovery/UDPdiscovery/Main.cpp
+++ UDPdiscovery/UDPdiscovery/Main.cpp
@@ -70,41 +70,12 @@ void client() {
 	//start communication
 	while (1)
 	{
-		/*printf("Enter message : ");
-		fgets(message, sizeof(message), stdin);
 		//send the message
 		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
 		}
-		//receive a reply and print it
-		//clear the buffer by filling null, it might have previously received data
-		memset(buf, '\0', BUFLEN);*/
-
-
-		//send the message
-		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
-		{
-			printf("sendto() failed with error code : %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
-		}
-		/*
-		//receive a reply and print it
-		//clear the buffer by filling null, it might have previously received data
-		memset(buf, '\0', BUFLEN);
-		//try to receive some data, this is a blocking call
-		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
-		{
-			printf("recvfrom() failed with error code : %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
-		}
-
-		puts(buf);
-
-
-		printf("IP address is: %s\n", inet_ntoa(si_other.sin_addr));
-		printf("port is: %d\n", (int)ntohs(si_other.sin_port));*/
 		Sleep(1000);
 	}
 
