57dc0dbe9b77b0c739614ad1f713c91457d5f310
MirkoCr <90mirko90@gmail.com>
2018-09-11 16:26:43 +0200

    ready to try Condition Variable

--- UDPdiscovery/UDPdiscovery/Main.cpp
+++ UDPdiscovery/UDPdiscovery/Main.cpp
@@ -1,36 +1,29 @@
-//aggiungere un set o un map per salvare gli utenti che hanno risposto. o magari
-//inserirli in un set di oggetti della classe utenti. qualcosa del genere.
-//usare le condition varible, per passare, al thread principale, ogni volta che si trova un nuovo 
-//utente. 
-
 
 /* oltre al nome utente potremmo iserire nel messaggio inviato anche un "flag" per identificare la modalità on line o of line. 
 oppure semplicemente non invia il messaggio. In questo caso bisogna gestire la possibilità 
-di attivare o disattivare l'invio del messagio. Vedere le specifiche per questo. 
-
-per poter passare il boolean condiviso tra i tread, si potrebbe prendere come riferimento l'esempio
-a slide 6 dell'argomento condition variable in cui si parla di atomic. Nel notro caso il sender invia solo se 
-l'atomic boolean è a true. */
+di attivare o disattivare l'invio del messaggio. Vedere le specifiche per questo.  */
 
 #include<thread> 
-#include<stdio.h>
 #include<winsock2.h>
+#include<iostream>
 
 
 
-#pragma warning(disable:4996) // da modificare ############################## togliere
+#pragma warning(disable:4996) // magari da modificare.
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
-#define SERVER "192.168.1.255"  //ip address of udp server
+#define SERVER "192.168.1.255"  /*ip address of udp server. funziona ma da rivedere. 
+Se cambio sottorete, potrebbe cambiare. dovrei quindi ogni volta recuperare 
+il mio IP e settare 255 come ultima cifra? O c'è un altro modo?*/
 #define BUFLEN 512  //Max length of buffer
 #define PORT 8888   //The port on which to listen for incoming data
 
 
-void Sender(); //invia il suo User Name ogni secondo in broadcast per sapere chi è online. 
-			   //se necessario potrebbe inviare una stringa con le informazioni necessarie come
-			   // un flag per dire se on line o no ecc.....
-void Receiver(); // resta in ascolto delle informazioni.
+void Sender(); /*invia il suo User Name ogni secondo in broadcast per sapere chi è online.
+			   se necessario potrebbe inviare una stringa con le informazioni necessarie 
+			   come un flag per dire se on line o no ecc....*/
+void Receiver(); /* resta in ascolto delle informazioni, riceve ip e username.*/
 
 
 
@@ -55,22 +48,25 @@ void Sender() {
 	struct sockaddr_in si_other;
 	int s, slen = sizeof(si_other);
 	char buf[BUFLEN];
-	char message[BUFLEN] = "Mirko";
-	WSADATA wsa;
+	char message[BUFLEN] = "Mirko"; /* messaggio da inviare. qui dobbiamo impostare le informazioni 
+									che vogliamo inserire nel payload: username, on-line o no, foto(?).*/
+	WSADATA wsa;					 
 
 	//Initialise winsock
-	printf("\nInitialising Winsock...");
+	std::cout << "\nInitialising Winsock..." << std::endl;
+
+	//printf("\nInitialising Winsock...");
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
+		std::cout<<"Failed. Error Code :"<< WSAGetLastError();
 		exit(EXIT_FAILURE);
 	}
-	printf("Initialised.\n");
+	std::cout<<"Initialised."<<std::endl;
 
 	//create socket
 	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
 	{
-		printf("socket() failed with error code : %d", WSAGetLastError());
+		std::cout << "socket() failed with error code : "<< WSAGetLastError();
 		exit(EXIT_FAILURE);
 	}
 
@@ -80,13 +76,14 @@ void Sender() {
 	si_other.sin_port = htons(PORT);
 	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 
+
 	//start communication
 	while (1)
 	{
 		//send the message
 		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
 		{
-			printf("sendto() failed with error code : %d", WSAGetLastError());
+			std::cout<<"sendto() failed with error code: "<< WSAGetLastError();
 			exit(EXIT_FAILURE);
 		}
 		Sleep(1000);
@@ -120,20 +117,20 @@ void Receiver()
 
 
 	//Initialise winsock
-	printf("\nInitialising Winsock...");
+	std::cout<< std::endl <<"Initialising Winsock...";
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
+		std::cout << "Failed. Error Code : "<< WSAGetLastError();
 		exit(EXIT_FAILURE);
 	}
-	printf("Initialised.\n");
+	std::cout << "Initialised." << std::endl;
 
 	//Create a socket
 	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
-		printf("Could not create socket : %d", WSAGetLastError());
+		std::cout << "Could not create socket : "<< WSAGetLastError();
 	}
-	printf("Socket created.\n");
+	std::cout << "Socket created.\n";
 
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
@@ -143,15 +140,15 @@ void Receiver()
 	//Bind
 	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
 	{
-		printf("Bind failed with error code : %d", WSAGetLastError());
+		std::cout << "Bind failed with error code : "<< WSAGetLastError();
 		exit(EXIT_FAILURE);
 	}
-	puts("Bind done");
+	std::cout << "Bind done" << std::endl;
 
-	//keep listening for data
+	//keep listening for data 
 	while (1)
 	{
-		printf("Waiting for data...");
+		std::cout << "Waiting for data...";
 		fflush(stdout);
 
 		//clear the buffer by filling null, it might have previously received data
@@ -160,13 +157,13 @@ void Receiver()
 		//try to receive some data, this is a blocking call
 		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
 		{
-			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			std::cout << "recvfrom() failed with error code : " << WSAGetLastError();
 			exit(EXIT_FAILURE);
 		}
 
 		//print details of the client/peer and the data received
-		printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-		printf("Data: %s\n", buf);
+		std::cout << "Received packet from "<< inet_ntoa(si_other.sin_addr)<<" : "<< ntohs(si_other.sin_port)<< std::endl;
+		std::cout << "Data: " << buf << std::endl;
 		//onLine.insert(inet_ntoa(si_other.sin_addr));
 		
 
